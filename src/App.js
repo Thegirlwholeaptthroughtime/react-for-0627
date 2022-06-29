52573f02f81b3967f5e0ba52c861f0be5def26fe
kang <kdh516094@gmail.com>
2022-06-29 18:01:43 +0900

    0629

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
-import Button from "./Button";
-import styles from "./App.module.css"
+import { func } from "prop-types";
 import { useState, useEffect } from "react";
 
-function App() {
 
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("")
-  const onClick = () => setValue((prev) => prev + 1)
-  const onChange = (event) => setKeyword(event.target.value)
-  console.log('i run all the time')
-  const iRunOnlyOnce = () =>{
-    console.log("once")
+
+function App(){
+  const [toDo, setToDo] = useState("")
+  const [toDos, setToDos] = useState([])
+  const onChange = (event) =>setToDo(event.target.value)
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if(toDo === ""){
+      return;
+    }
+    setToDo("");
   }
-  useEffect(iRunOnlyOnce, [])
-  console.log("search for", keyword)
-  return (
-    <div>
-      <input value={keyword} onChange={onChange} type="text" placeholder="Search here..."></input>
-      <h1 className={styles.title}>
-         {counter}
-      </h1>
-      <button onClick={onClick}>click me</button>
-    </div>
-  );
-}
+  return(
+      <div>
+        <form onSubmit={onSubmit}>
+          <input onChange={onChange} type="text" placeholder="write your to do"></input>
+          <button>Add To Do</button>
+        </form>
+      </div>
+      )
+    }
+
 
 export default App;
