6ff51da9d40ad4a7489e4bdb5dec3e6bf6db5521
Thegirlwholeaptthroughtime <kdh516094@gmail.com>
2022-07-03 20:17:59 +0900

    coin

--- src/App.js
+++ src/App.js
@@ -1,28 +1,36 @@
-import { func } from "prop-types";
-import { useState, useEffect } from "react";
 
-
-
+import { useEffect, useState } from "react";
+    
 function App(){
-  const [toDo, setToDo] = useState("")
-  const [toDos, setToDos] = useState([])
-  const onChange = (event) =>setToDo(event.target.value)
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if(toDo === ""){
-      return;
-    }
-    setToDo("");
-  }
-  return(
-      <div>
-        <form onSubmit={onSubmit}>
-          <input onChange={onChange} type="text" placeholder="write your to do"></input>
-          <button>Add To Do</button>
-        </form>
-      </div>
-      )
-    }
+   const [loading, setLoading] = useState(true);
+
+   const [coins, setCoins] = useState([]); 
+   useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      })
+   }, [])
+
+   return(
+     <div>
+        <h1>The coins! {loading ? "" : `(${coins.length})`}</h1>
+        {loading ? <strong>loading...</strong>: 
+          <select>
+          {coins.map((coin) => (
+              <option>
+                {coin.name} ({coin.symbol}) : ${coin.quotes.USD.price} USD
+              </option>
+          ))}
+        </select>
+        
+        }
+        
+     </div>
+   )
+}
 
 
 export default App;
